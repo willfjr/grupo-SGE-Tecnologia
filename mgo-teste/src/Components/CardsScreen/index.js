bf64f3c62c528f20fd813c34b8c0b5b2b2f7b810
willfjr <willfjr@icloud.com>
2021-06-05 20:44:00 -0300

    Cria função para realizar a requisição da API de Tags

--- mgo-teste/src/Components/CardsScreen/index.js
+++ mgo-teste/src/Components/CardsScreen/index.js
@@ -1,13 +1,15 @@
-import React from "react";
-
+import React, { useState } from "react";
 import Card from "../CollapsibleCard";
+import useRequestData from "../../Hooks/useRequestData";
 
 const CardsScreen = () => {
-  return (
-    <>
-      <Card />
-    </>
-  );
+  const [names] = useRequestData([], "https://cataas.com/api/tags");
+
+  const renderCards = names.map((name) => {
+    return <Card name={name} key={name} />;
+  });
+
+  return <>{renderCards}</>;
 };
 
 export default CardsScreen;
--- mgo-teste/src/Components/CollapsibleCard/index.js
+++ mgo-teste/src/Components/CollapsibleCard/index.js
@@ -1,21 +1,13 @@
-import useRequestData from "../../Hooks/useRequestData";
 import { useState } from "react";
 
-const Card = () => {
-  const [cats] = useRequestData([], "https://cataas.com/api/tags");
-
+const Card = (props) => {
   const [isOpen, setIsOpen] = useState(false);
-
-  const titles = cats.map((tags, index, array) => {
-    return tags;
-  });
   return (
-    <>
-      <div className="card">
-        <button onClick={() => setIsOpen(!isOpen)}>+</button>
-        {isOpen && <div>Content</div>}
-      </div>
-    </>
+    <div className="card">
+      {props.tag}
+      <button onClick={() => setIsOpen(!isOpen)}>{props.name}</button>
+      {isOpen && <div>Content</div>}
+    </div>
   );
 };
 
